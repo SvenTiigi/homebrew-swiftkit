e11fc6dd1604c964b4befe370b6174ea87da0148
Sven Tiigi <sven.tiigi@gmail.com>
2019-04-30 09:38:54 +0200

    Update swiftkit.rb

--- swiftkit.rb
+++ swiftkit.rb
@@ -1,8 +1,8 @@
 class Swiftkit < Formula
-    desc "Start your next Open-Source Swift Framework"
+    desc "Start your next Open-Source Swift Framework 📦"
     homepage "https://github.com/SvenTiigi/SwiftKit"
-    url "https://github.com/SvenTiigi/SwiftKit/archive/1.0.0.tar.gz"
-    sha256 "e30269929901c75d2f0f20a188336cf4e6b18e41eeb38fb45b10c73dd6c91944"
+    url "https://github.com/SvenTiigi/SwiftKit/archive/1.1.0.tar.gz"
+    sha256 "0db259c2b0841637eb5ebf27707278533c4394cab2ad5001de219151abdbee46"
     head "https://github.com/SvenTiigi/SwiftKit.git"
 
     depends_on :xcode => ["8.0", :build]
