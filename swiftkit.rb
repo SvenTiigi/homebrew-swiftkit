4df47991b51b50230fd5f04d9084ab76f03ce1a5
Sven Tiigi <sven.tiigi@gmail.com>
2021-09-30 20:48:26 +0200

    SwiftKit release 1.3.8 🚀

--- swiftkit.rb
+++ swiftkit.rb
@@ -1,8 +1,8 @@
 class Swiftkit < Formula
     desc "Start your next Open-Source Swift Framework 📦"
     homepage "https://github.com/SvenTiigi/SwiftKit"
-    url "https://github.com/SvenTiigi/SwiftKit/archive/1.3.7.tar.gz"
-    sha256 "de041932b7f5ff4e8dbd6d6250af8cd28046290b7415be04d1b3e183f0df5f45"
+    url "https://github.com/SvenTiigi/SwiftKit/archive/1.3.8.tar.gz"
+    sha256 "3b83a47ce880203446c5729688a195f70a839ccf1fbc0d763e770f1d9ad771bc"
     head "https://github.com/SvenTiigi/SwiftKit.git"
 
     depends_on :xcode => ["8.0", :build]
